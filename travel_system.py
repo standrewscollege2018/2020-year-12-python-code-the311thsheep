a506384fdd5ae03168b14b1ad73f0249a94c581f
the311thsheep <the311thsheep@users.noreply.github.com>
2020-02-21 15:20:17 +1300

    initail
    21/2/2020

---
+++ travel_system.py
@@ -0,0 +1,18 @@
+#travel system
+#21/02/2020
+
+#list of starting points
+#start_point_list[place][connecting_flight_cost]
+start_point_list = [["Auckland", 0], ["Wellinton", 50], ["Christchurch", 75]]
+
+#destinations list
+#destination_list[place][flight_cost]
+destination_list = [["Sydney", 326], ["Tonga", 378], ["Shanghai", 1436], ["London", 2376]]
+
+#accomodation list
+#accomodation_list[place][cost]
+accomodation_list = [["Sydney", 120], ["Tonga", 40], ["Shanghai", 60], ["London", 80]]
+
+#user choices list
+choices_list = []
+
