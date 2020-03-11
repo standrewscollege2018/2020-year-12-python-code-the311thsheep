158444aca162885f0c7e681fadacd0b226d4aa1b
the311thsheep <the311thsheep@users.noreply.github.com>
2020-03-12 09:21:28 +1300

    udate - errors fixes

--- resturant_task.py
+++ resturant_task.py
@@ -24,16 +24,20 @@ def print_list_options(l):
         print(i+1, ") ", l[i][0], ", $", l[i][1])
         
 """print specials options"""
-def print_specials_options(l):
-    l_specials = 0
+def print_specials_options(l, course_specials_list):
+    l_specials_len = 0
     print ("SPECIALS")
-    for i in range(0, len(specials_list)):
-        if specials_list[i][1] == l and specials_list[i][0] == True:
-            print(i+len(l)+1, specials_list[i][2], ", $", specials_list[i][3])
-            l_specials = l_specials+1
-    return l_specials
+    for i in range(0, len(course_specials_list)):
+        #if special is currently available
+        if course_specials_list[i][0] == True:
+            #prints index and name of special and price
+            print(i+len(l)+1, ") ", course_specials_list[i][1], ", $", course_specials_list[i][2])
+            #records length of available specials in this course
+            l_specials_len = l_specials_len+1
+    return l_specials_len
         
         
+
 
 #list of entrees ["dish name", price]
 entrees_list = [["Nachos", 12], ["Rice", 15], ["Apple Slices", 2]]
@@ -43,50 +47,66 @@ mains_list = [["burger", 20], ["Soup", 22], ["Toastie", 18]]
 deserts_list = [["Ice cream", 10], ["Cake", 13],["Cheesecake", 15]]
 
 
-#list of specials[is_available, what_course, dish_name, price]
-specials_list = [[True, entrees_list, "Cerial of the day", 15]]
-        
-entrees = []
+#list of specials[is_available, dish_name, price]
+entree_specials_list = [[True, "Cerial of the day", 15]]
+mains_specials_list = [[True, mains_list, "soup of the day", 16]]
+desserts_specials_list = []
+
+#list order is saved to [entrees], [mains], [desserts]  
+order = [["entrees"], ["mains"],["deserts"]]
+
+
+ENTREES = True    
     
-    
-while True:
-    #asks user to enter order or enter specials
-    ask = get_correct_input("What would you like to do?\n 1) Enter order\n 2) Edit specials\n>>>: ", 2)
+
+
      
-    #enter order 
+
+
+ask = get_correct_input("What would you like to do?\n 1) Enter order\n 2) Edit specials\n>>>: ", 2)
+#enter order 
+if ask == 0:
+    print ("What course?\n 1) Entree\n 2) Main\n 3) Desert")
+    ask = get_correct_input(">>>: ", 3)
+    
+    
+    #asks user what entrees they want
+
     if ask == 0:
-        print ("What course?\n 1) Entree\n 2) Main\n 3) Desert")
+        print ("current entries: ", order[0])
+    #prints entrees 
+        print_list_options(entrees_list)
+    #prints availables entree specials and assigns number of available entree specials to variable 'len_entree_specials'
+        len_entree_specials = print_specials_options(entrees_list, entree_specials_list)
+    #asks user and assigns user choice to variable 'ask'
+        entrees_input = get_correct_input(">>>: ", len(entrees_list)+len_entree_specials)
+    #aks user how many of this item they would like 
+        quantity = get_correct_input("How many of these would you like?\n>>>: ", 5)
+    
+    
+        print(" 1)Confirm order\n 2) Order more entriees\n 3) Cancel order")
         ask = get_correct_input(">>>: ", 3)
-        
-        
-        #asks user what entrees they want 
-        if ask == 0:
-        #prints entrees 
-            print_list_options(entrees_list)
-        #prints availables entree specials and assigns number of available entree specials to variable 'len_entree_specials'
-            len_entree_specials = print_specials_options(entrees_list)
-        #asks user and assigns user choice to variable 'ask'
-            entrees_input = get_correct_input(">>>: ", len(entrees_list)+len_entree_specials)
-        #aks user how many of this item they would like 
-            quantity = get_correct_input("How many of these would you like?\n>>>: ", 5)
-        
-        
-            print(" 1)Confirm order\n 2) Order more entriees\n 3) Cancel order")
-            ask = get_correct_input(">>>: ", 3)
-            if ask == 0:
-                #assigns to entriees list
-                entrees.append([entrees_list[entrees_input], quantity])
-                print (entrees)
-                break
-            elif ask == 1:
-                continue
-           
-                
-            
+        if ask == 0 and ENTREES == True:
+            #assigns to order list
+            #if order is not a special
+            if entrees_input < len_entree_specials:
+                order[0].append([entrees_list[entrees_input-1], quantity])
+            else:
+                order[0].append([entree_specials_list[entrees_input-len(entrees_list)-1], quantity])
         elif ask == 1:
-            print()
+            order[0].append([entrees_list[entrees_input], quantity])
+            ENTREES = False
+       
+            
+    #asks user what mains they want    
+    elif ask == 1:
+        print()
         
-        
-    #edit specials
-    if ask == 1:
-        print ("Enter username ")
+print ("your order is :\n")
+print ("ENTREES:")
+for i in range(0, len(order[0]))
+
+
+#edit specials
+if ask == 1:
+    print ("Enter username ")
